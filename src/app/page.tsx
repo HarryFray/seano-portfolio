cd4308994e9416791d2f532d90fc1ca1ab6f82eb
nfray <harry.fray7@gmail.com>
2023-07-19 11:12:30 -0600

    update background on hover

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,50 +1,69 @@
+"use client";
+import React, { useState } from "react";
 import Link from "next/link";
-import Image from "next/image";
+import Image, { StaticImageData } from "next/image";
 
 import TestBackgroundIMG from "../../public/test-background.jpg";
 
-const VIDEOGRAPHY_PROJECTS = [
+interface Project {
+  title: string;
+  path: string;
+  backgroundImage: StaticImageData;
+}
+
+const VIDEOGRAPHY_PROJECTS: Project[] = [
   {
     title: "Ethereal Elopement",
     path: "ethereal_elopement",
+    backgroundImage: TestBackgroundIMG,
   },
   {
     title: "Pixelated Dreams",
     path: "pixelated_dreams",
+    backgroundImage: TestBackgroundIMG,
   },
   {
     title: "Wanderlust Chronicles",
     path: "wanderlust_chronicles",
+    backgroundImage: TestBackgroundIMG,
   },
   {
     title: "Rhythm of the Streets",
     path: "rhythm_of_the_streets",
+    backgroundImage: TestBackgroundIMG,
   },
   {
     title: "Gadgetopia Unveiled",
     path: "gadgetopia_unveiled",
+    backgroundImage: TestBackgroundIMG,
   },
   {
     title: "Voices of Change",
     path: "voices_of_change",
+    backgroundImage: TestBackgroundIMG,
   },
   {
     title: "Runway Enchantment",
     path: "runway_enchantment",
+    backgroundImage: TestBackgroundIMG,
   },
   {
     title: "Victory Symphony",
     path: "victory_symphony",
+    backgroundImage: TestBackgroundIMG,
   },
 ];
 
 const Home = () => {
+  const [currentProject, setCurrentProject] = useState(VIDEOGRAPHY_PROJECTS[0]);
+
   return (
     <main className="min-h-screen">
       <div className="p-4">
+        <h1>{currentProject.title}</h1>
         <Image
           alt="some image"
-          src={TestBackgroundIMG}
+          src={currentProject.backgroundImage}
           style={{ zIndex: -1 }}
           layout="fill"
           objectFit="cover"
@@ -57,8 +76,9 @@ const Home = () => {
           {VIDEOGRAPHY_PROJECTS.map(({ title, path }, i) => {
             return (
               <Link
-                className="w-fit text-base font-semibold hover:underline cursor-pointer"
                 key={`${path}${i}`}
+                onMouseEnter={() => setCurrentProject(VIDEOGRAPHY_PROJECTS[i])}
+                className="w-fit text-base font-semibold hover:underline cursor-pointer"
                 href={`/seano-proj/${path}`}
               >
                 {title}
