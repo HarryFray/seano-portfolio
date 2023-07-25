929cabe38322bd256bffdd1bd06e2707bdc4c1c9
nfray <harry.fray7@gmail.com>
2023-07-25 09:23:56 -0600

    pulling projects specifically

--- app/[project_id]/page.tsx
+++ app/[project_id]/page.tsx
@@ -1,9 +1,30 @@
-import Link from "next/link";
+import peformRequest from "../lib/datocms";
 
-const Project = ({ params }: { params: { project_id: string } }) => {
+const projectQuery = (slug: string) => {
+  return `
+    query {
+      project(filter: { slug: { eq: "${slug}" } }) {
+        title
+        projectVideo
+      }
+    }
+  `;
+};
+
+interface IProjectProps {
+  params: {
+    project_id: string;
+  };
+}
+
+const Project = async ({ params }: IProjectProps) => {
+  const { data } = await peformRequest({
+    query: projectQuery(params.project_id),
+  });
+
   return (
     <main className="min-h-screen p-40">
-      <h1>My Slug: {params.project_id}</h1>
+      <h1>{data.project.title}</h1>{" "}
     </main>
   );
 };
