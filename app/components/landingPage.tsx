2ef7719c7cac882614dfe4ce12ee82ef820dd028
nfray <harry.fray7@gmail.com>
2023-07-26 14:50:50 -0600

    image transitions

--- app/components/landingPage.tsx
+++ app/components/landingPage.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, Fragment } from "react";
 import Link from "next/link";
 import Image from "next/image";
 import { IProject } from "../constants";
@@ -10,47 +10,53 @@ interface IProps {
 
 const Home = ({ allProjects }: IProps) => {
   const [currentProject, setCurrentProject] = useState(allProjects[0]);
+  const [prevProject, setPrevProject] = useState(allProjects[0]);
 
   const [showCurrentGif, setShowCurrentGif] = useState(false);
 
-  const [showCurrentBackground, setShowCurrentBackground] = useState(false);
-
   useEffect(() => {
     setShowCurrentGif(false);
-    setTimeout(() => setShowCurrentGif(true), 1000);
-
-    setShowCurrentBackground(false);
-    setTimeout(() => setShowCurrentBackground(true), 1);
+    setTimeout(() => setShowCurrentGif(true), 300);
   }, [currentProject]);
 
   return (
     <main className="min-h-screen">
-      {showCurrentBackground && (
-        <Image
-          style={{ zIndex: -1, animation: "fadeinbackgroundimg 1.5s" }}
-          src={currentProject?.landingBackground?.responsiveImage?.src}
-          layout="fill"
-          objectFit="cover"
-          quality={100}
-          alt={`${currentProject.title} background image`}
-          // placeholder="blur"
-          // blurDataURL={
-          //   currentProject?.landingBackground?.responsiveImage?.base64
-          // }
-        />
-      )}
       <div className="mx-40 flex items-center justify-between min-h-screen">
         <div className="w-fit flex flex-col">
-          {allProjects.map(({ title, slug }, i) => {
+          {allProjects.map(({ title, slug, landingBackground, id }, i) => {
+            const isCurrentProject = id === currentProject.id;
+            const isPrevProject = id === prevProject.id;
+
             return (
-              <Link
-                key={`${slug}${i}`}
-                onMouseEnter={() => setCurrentProject(allProjects[i])}
-                className="w-fit text-base font-semibold hover:line-through mb-1.5 text-white "
-                href={slug}
-              >
-                {title}
-              </Link>
+              <Fragment key={`${slug}${i}`}>
+                <Image
+                  style={{
+                    zIndex: isCurrentProject ? -1 : -2,
+                    animation: isCurrentProject
+                      ? "fadeinbackgroundimg 1s"
+                      : "fadeoutbackgroundimg 1s",
+                    display:
+                      !isCurrentProject && !isPrevProject ? "none" : "block",
+                  }}
+                  src={landingBackground?.responsiveImage?.src}
+                  layout="fill"
+                  objectFit="cover"
+                  quality={100}
+                  alt={`${title} background image`}
+                  // placeholder="blur"
+                  // blurDataURL={
+                  //   currentProject?.landingBackground?.responsiveImage?.base64
+                  // }
+                />
+                <Link
+                  onMouseEnter={() => setCurrentProject(allProjects[i])}
+                  onMouseLeave={() => setPrevProject(allProjects[i])}
+                  className="w-fit text-base font-semibold hover:line-through mb-1.5 text-white "
+                  href={slug}
+                >
+                  {title}
+                </Link>
+              </Fragment>
             );
           })}
         </div>
@@ -61,7 +67,7 @@ const Home = ({ allProjects }: IProps) => {
             quality={100}
             width={500}
             height={500}
-            style={{ animation: "fadein 2s" }}
+            style={{ animation: "fadein 2.5s" }}
           />
         )}
       </div>
