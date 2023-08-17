ec5fafcdd016069fd919b0b499d71ca1831c3b34
nfray <harry.fray7@gmail.com>
2023-08-17 10:12:18 -0500

    experiment only using QuerySub

--- app/project/[project_id]/page.tsx
+++ app/project/[project_id]/page.tsx
@@ -1,7 +1,8 @@
-import peformRequest from "../../global/datocms";
+"use client";
 import { Project } from "../../global/globalStore";
 import ProjectPage from "../../components/projectPage";
 import { buildProjectQuery, PROJECTS_QUERY } from "../../global/queries";
+import { useQuerySubscription } from "react-datocms";
 
 interface projectProps {
   params: {
@@ -9,14 +10,22 @@ interface projectProps {
   };
 }
 
-const Project = async ({ params }: projectProps) => {
-  const { data: projectData } = await peformRequest({
+const Project = ({ params }: projectProps) => {
+  const { data: projectData } = useQuerySubscription({
+    enabled: true,
     query: buildProjectQuery(params.project_id),
+    token: process.env.NEXT_PUBLIC_DATOCMS_API_TOKEN as string,
   });
 
-  const { data: allProjectsData } = await peformRequest({
+  const { data: allProjectsData } = useQuerySubscription({
+    enabled: true,
     query: PROJECTS_QUERY,
+    token: process.env.NEXT_PUBLIC_DATOCMS_API_TOKEN as string,
   });
+
+  if (!projectData || !allProjectsData) {
+    return null;
+  }
 
   const allProjects: Project[] = allProjectsData.allProjects;
 
