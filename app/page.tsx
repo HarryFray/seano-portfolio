7cfabeffda922abce87f8e7b7d531e26b920d89d
nfray <harry.fray7@gmail.com>
2023-08-17 09:43:26 -0500

    poc complete

--- app/page.tsx
+++ app/page.tsx
@@ -1,59 +1,24 @@
 "use client";
-import { useEffect } from "react";
 import LandingPage from "./components/landingPage";
-import peformRequest from "./global/datocms";
 import { Project } from "./global/globalStore";
-import { PROJECTS_QUERY, SEAN_INFO_QUERY } from "./global/queries";
+import { PROJECTS_QUERY } from "./global/queries";
 import { useQuerySubscription } from "react-datocms";
 
 const apiToken = process.env.NEXT_PUBLIC_DATOCMS_API_TOKEN;
 const Home = () => {
-  const { status, error, data } = useQuerySubscription({
+  const { status, data } = useQuerySubscription({
     enabled: true,
-    query: `{
-      seanInfo {
-        workImage {
-          responsiveImage {
-            src
-          }
-        }
-      }
-    }`,
-    variables: { first: 10 },
-    token: apiToken || "",
+    query: PROJECTS_QUERY,
+    token: apiToken as string,
   });
 
-  const statusMessage = {
-    connecting: "Connecting to DatoCMS...",
-    connected: "Connected to DatoCMS, receiving live updates!",
-    closed: "Connection closed",
-  };
+  if (status === "connecting" || !data) {
+    return null;
+  }
 
-  console.log({ data });
+  const allProjects: Project[] = data.allProjects;
 
-  useEffect(() => {
-    console.log({ data });
-    console.log({ error });
-    console.log({ status });
-  }, [status]);
-
-  return (
-    <div>
-      <p className="text-white">Connection status: {statusMessage[status]}</p>
-      {error && (
-        <div>
-          <h1 className="text-white">Error: {error.code}</h1>
-          <div className="text-white">{error.message}</div>
-          {error.response && (
-            <pre className="text-white">
-              {JSON.stringify(error.response, null, 2)}
-            </pre>
-          )}
-        </div>
-      )}
-      <h1 className="text-white">{JSON.stringify(data)}</h1>
-    </div>
-  );
+  return <LandingPage allProjects={allProjects} />;
 };
 
 export default Home;
