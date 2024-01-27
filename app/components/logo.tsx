9fef82382c71c53c0b86477fa339ea6f2fd394f9
nfray <harry.fray7@gmail.com>
2024-01-27 14:46:09 -0700

    logo updates

--- app/components/logo.tsx
+++ app/components/logo.tsx
@@ -2,15 +2,19 @@ import Link from "next/link";
 
 const RootLayout = () => {
   return (
-    <Link
-      href="/"
-      className="text-2xl font-bold text-white hover:line-through
-            md:text-4xl md:w-auto md:inline w-full"
-    >
-      SEAN
-      <br className="xl:hidden" />
-      O’NEILL
-    </Link>
+    <div className="flex items-center">
+      <Link
+        href="/"
+        className="text-2xl font-bold text-white hover:line-through
+      md:text-4xl md:w-auto md:flex items-center w-full"
+      >
+        SEANO
+        <br className="xl:hidden" />
+      </Link>
+      <span className="text-lg pl-3 text-white font-light">
+        Director & Cinematographer
+      </span>
+    </div>
   );
 };
 
