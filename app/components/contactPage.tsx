4bc2406bf7e7724838a64f7322e45cb1c9061d53
nfray <harry.fray7@gmail.com>
2024-01-27 15:20:58 -0700

    link contact page to dato

--- app/components/contactPage.tsx
+++ app/components/contactPage.tsx
@@ -5,12 +5,12 @@ interface contactPageProps {
   seanInfo: seanInfo;
 }
 
-interface socialLinke {
+interface socialLink {
   text: string;
   href: string;
 }
 
-const socialLinks: socialLinke[] = [
+const socialLinks: socialLink[] = [
   {
     text: "hi@SEANOfilms.com",
     href: "mailto:hi@SEANOfilms.com",
@@ -31,43 +31,16 @@ const socialLinks: socialLinke[] = [
 
 const ContactPage = ({ seanInfo }: contactPageProps) => {
   return (
-    <main className="min-h-screen p-10 lg:p-40">
-      <p className="mt-12 text-white">
-        Somewhere between wonder and memory, lies the most complex vitality —
-        that which makes us human. A system of light and sound, feeling and
-        color. There’s an intersection found here, that collectively reminds us
-        we’re alive. For me, the moving image awakens that vitality, and at its
-        core, contains the power to make us feel.
-      </p>
-      <p className="mt-4 text-white">
-        In 2019 I moved my home-base from New York City to Denver, to reconnect
-        with the things I enjoy the most. My pursuit is driven by an exposition
-        of the incredible stories we all chase, and the places between where we
-        land our feet.
-      </p>
-      <div
-        className="flex flex-col mt-4 justify-between align-top 
-          lg:mt-20 lg:flex-row"
-      >
-        <div className="flex flex-col text-white mb-4">
-          {socialLinks.map(({ text, href }, index) => (
-            <a
-              key={index}
-              className="w-fit text-base font-semibold hover:line-through mb-1.5"
-              href={href}
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              {text}
-            </a>
-          ))}
-        </div>
-        <Image
-          src={seanInfo.workImage.webp}
-          alt="Sean O'Neill at work"
-          width={400}
-          height={400}
-        />
+    <main className="min-h-screen p-10 lg:p-40 flex flex-col items-center justify-center">
+      <Image
+        src={seanInfo.workImage.webp}
+        alt="Sean O'Neill at work"
+        width={400}
+        height={400}
+        className="mb-8"
+      />
+      <div className="text-white text-center whitespace-pre-wrap leading-5 font-light">
+        {seanInfo.contactPageText}
       </div>
     </main>
   );
