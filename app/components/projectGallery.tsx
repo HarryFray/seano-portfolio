11761edc380b2317d8147a6921c3250c35950519
nfray <harry.fray7@gmail.com>
2024-04-22 12:05:58 -0600

    progress

--- app/components/projectGallery.tsx
+++ app/components/projectGallery.tsx
@@ -4,6 +4,8 @@ import Image from "next/image";
 import { FaChevronDown } from "react-icons/fa";
 import { FaPlay } from "react-icons/fa";
 import { Project } from "../global/globalStore";
+import { RemoveScroll } from "react-remove-scroll";
+// import { IoClose } from "react-icons/io5";
 
 const checkIfElementIsInViewport = (element: Element | null): boolean => {
   if (!element) return false;
@@ -57,22 +59,27 @@ const ProjectGallery = ({
   return (
     <>
       {fullScreenVideoLink && (
-        <div
-          onClick={() => setFullScreenVideoLink("")}
-          className="fixed flex items-center justify-center w-screen h-screen top-0 left-0"
-        >
-          <iframe
-            src={fullScreenVideoLink}
-            allow="autoplay; fullscreen; picture-in-picture"
-            allowFullScreen
-            style={{
-              animation: "fadein 1s",
-              width: iFrameHeight * 2.2,
-              height: iFrameHeight * 1.4,
-              zIndex: 1000,
-            }}
-          />
-        </div>
+        <RemoveScroll>
+          <div
+            onClick={() => setFullScreenVideoLink("")}
+            className="fixed flex items-center justify-center w-screen h-screen top-0 left-0 bg-black z-10"
+          >
+            {/* <span className="text-black lg:text-4xl  cursor-pointer absolute right-32 top-24 z-100000">
+              <IoClose onClick={() => setFullScreenVideoLink("")} />
+            </span> */}
+            <iframe
+              src={fullScreenVideoLink}
+              allow="autoplay; fullscreen; picture-in-picture"
+              allowFullScreen
+              style={{
+                animation: "fadein 1s",
+                width: iFrameHeight * 2.2,
+                height: iFrameHeight * 1.4,
+                zIndex: 1000,
+              }}
+            />
+          </div>
+        </RemoveScroll>
       )}
       <div className="flex flex-col items-center" ref={galleryRef}>
         <span className="text-white mt-4 h-8 lg:text-4xl  cursor-pointer lg:mt-16 md:h-20">
@@ -94,10 +101,11 @@ const ProjectGallery = ({
           ))}
           {galleryVideos.map(({ webp, customData }, i) => {
             const { videoLink } = customData;
+            console.log({ videoLink });
             return (
               <div
                 key={i}
-                className="relative flex items-center justify-center hover:scale-105 transition-all"
+                className="relative flex items-center justify-center hover:scale-105 transition-all z-1"
               >
                 <span className="absolute text-white text-xl lg:text-4xl cursor-pointer z-1">
                   <FaPlay />
